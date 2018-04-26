8fe6c13751424af2da962c4408c60ed04dccd545
Shreya Shah <shah.shreya.3012@gmail.com>
2018-04-26 16:21:47 -0700

    Sorting Parameters added for revenue and search

--- kafka-back-end/services/movieschedule/getRevenueByMovie.js
+++ kafka-back-end/services/movieschedule/getRevenueByMovie.js
@@ -8,14 +8,14 @@ function handle_request(msg, callback) {
     console.log(`Incoming message: ${JSON.stringify(msg)}`);
     let condition = {};
     let query = "SELECT " +
-        " SUM(billings.total_price) as total_revenue, movies.movie_name, movies.id " +
+        " SUM(billings.total_price) as total_revenue, movies.movie_name, movies.photos, movies.id " +
         " FROM `movie_schedules` as ms JOIN" +
         " billings ON ms.id = billings.movie_schedule_id LEFT JOIN" +
         " movies ON movies.id = ms.movie_id WHERE billings.status = 'A'";
     if (msg.movie_id) {
         query = query + "AND ms.movie_id = " + msg.movie_id
     }
-    query = query + " GROUP BY ms.movie_id;"
+    query = query + " GROUP BY ms.movie_id ORDER BY total_revenue desc;"
     console.log(`Incoming Query message:`, query);
     db.sequelize.query(query, { type: db.sequelize.QueryTypes.SELECT })
         .then((movieRevenueList) => {
