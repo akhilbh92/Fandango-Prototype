bace8f8918bf612e1d7ff7f74b50c388bc867f05
rachitchokshi <rachitchokshi@gmail.com>
2018-04-28 01:22:09 -0700

    -enable the admin user to view and modify other user's profile

--- fandango-backend/routes/users/updateProfile.js
+++ fandango-backend/routes/users/updateProfile.js
@@ -4,9 +4,42 @@ var kafka = require('../../kafka/client');
 let resFormat = require("../../helpers/res_format");
 
 let updateProfileRouterFn = async function (req, res, next) {
-    const userId = req.session.passport.user.userId;
-    const body = req.body;
+
+    // const userId = req.session.passport.user.userId;
+    // const body = req.body;
+    // body["userId"] = userId;
+
+    let userId = req.session.passport.uyser.userId;
+    let body = req.body;
     body["userId"] = userId;
+
+    try{
+        if(req.user){
+            if(req.user.role === 1){
+                if(req.body.userId){
+                     body = req.body;
+                }else{
+                     userId = req.session.passport.user.userId;
+                     body = req.body;
+                    body["userId"] = userId;
+                }
+            }else{
+                 userId = req.session.passport.user.userId;
+                 body = req.body;
+                body["userId"] = userId;
+            }
+        }else{
+            const err = new Error("Session not found");
+            err.status = 403;
+            throw err;
+        }
+    }catch (e) {
+        let resObj = new resFormat(e);
+        return res.status(resObj.getStatus()).json(resObj.log());
+    }
+
+
+
 
     let updateProfileById = function (body) {
         return new Promise(function (resolve, reject) {
