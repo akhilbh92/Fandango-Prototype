919d7559b2e9d197a57460ff6ad46375fae5a6b6
rachitchokshi <rachitchokshi@gmail.com>
2018-04-29 16:55:34 -0700

    -converted movie tickets poster to dynamic

--- fandango-frontend/src/components/MovieTickets/MovieHallsBox/MovieHallsBox.js
+++ fandango-frontend/src/components/MovieTickets/MovieHallsBox/MovieHallsBox.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import './MovieHallsBox.css'
 
 class Movie_Halls extends Component{
+
     render(){
         return <div>
             <div id="movehallsDIV_1">
