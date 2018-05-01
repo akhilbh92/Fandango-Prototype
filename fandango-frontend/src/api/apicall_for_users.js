524b2f6f0cb50f6f1873da9b214bae2cb3e203d6
harshrp2511github <harshrp2511@gmail.com>
2018-05-01 03:39:42 -0700

    Merge branch 'master' of https://github.com/manogna-mujje/Fandango-Prototype
    # Conflicts:
    #	fandango-backend/userclicks.log

--- fandango-frontend/src/api/apicall_for_users.js
+++ fandango-frontend/src/api/apicall_for_users.js
@@ -186,3 +186,21 @@ export const getPurchaseHistory = (payload) =>
             console.log("This is error");
             return error;
         });
+
+export const getProfile = (payload) =>
+    fetch(`${api}/getProfile`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        credentials:'include',
+        body: JSON.stringify(payload)
+    }).then(res => res.json())
+        .then(res =>{
+            return res;
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
