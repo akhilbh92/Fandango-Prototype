e6c0ce792e713e9d737185727bc3d8a96b74007f
rachitchokshi <rachitchokshi@gmail.com>
2018-04-30 19:59:44 -0700

    -modified /getmovies webservice to return average ratings on each movie ONLY WHEN WHERE CONDITION IS NOT PASSED. otherwise it will return without avergae ratings field

--- kafka-back-end/services/movies/getMovies.js
+++ kafka-back-end/services/movies/getMovies.js
@@ -2,11 +2,27 @@ var modelMovie = require('./../../models/Movie');
 
 function handle_request(msg, callback){
     console.log(`Incoming message: ${JSON.stringify(msg)}`);
-    modelMovie.Movie.findAll({
-        where : [msg]
-    }).then((movie) => {
-        callback(null, movie)
-    });
+
+    if(Object.keys(msg).length !== 0){
+        modelMovie.Movie.findAll({
+            where : [msg]
+        }).then((movie) => {
+            callback(null, movie)
+        });
+    }else{
+        let sql = "select id,movie_name,description,see_it_in,trailer,photos,cast,movie_length,release_date,genres,t1.createdAt,t1.updatedAt,AVG(ratings.rating) AS avgrating " +
+            "FROM movies as t1 " +
+            "LEFT OUTER JOIN ratings on ratings.movie_id=t1.id " +
+            "GROUP BY t1.id";
+
+        try{
+            modelMovie.Movie.sequelize.query(sql).spread((results, metadata) => {
+                callback(null,results);
+            });
+        }catch (err) {
+            callback(err,null);
+        }
+    }
 }
 
 module.exports = {
