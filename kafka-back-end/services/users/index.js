bdf8c948d9b88eb77f80046bbcea71a538827d94
Shreya Shah <shah.shreya.3012@gmail.com>
2018-05-01 03:47:02 -0700

    Login User Email check

--- kafka-back-end/services/users/index.js
+++ kafka-back-end/services/users/index.js
@@ -3,20 +3,21 @@ const bcrypt = require('bcrypt');
 
 
 module.exports = {
-    getUserByEmail: async function (body,cb) {
+    getUserByEmail: async function (body, cb) {
         console.log("Inc Login handle request:" + JSON.stringify(body));
-        try{
+        try {
             const user = await usermodel.findAll({
-                where:{
-                    email:body.email,
+                where: {
+                    email: body.email,
+                    is_archive: false
                 }
             });
-            cb(null,user[0]);
-        }catch (e) {
-            cb(e,null);
+            cb(null, user[0]);
+        } catch (e) {
+            cb(e, null);
         }
     },
-    Signup: async function (body,cb) {
+    Signup: async function (body, cb) {
         try {
             const saltedhash = await bcrypt.hash(body.password, 10);
             const user = await usermodel.create({
@@ -25,86 +26,86 @@ module.exports = {
                 first_name: body.fname,
                 role: body.role
             });
-            cb(null,user)
-        }catch (e) {
-            cb(e,null);
+            cb(null, user)
+        } catch (e) {
+            cb(e, null);
         }
     },
-    getUserById: async function (body,cb) {
-        try{
+    getUserById: async function (body, cb) {
+        try {
             const user = await usermodel.findAll({
-                where:{
-                    userId:body.userId,
+                where: {
+                    userId: body.userId,
                 }
             });
-            cb(null,user[0]);
-        }catch (e) {
-            cb(e,null);
+            cb(null, user[0]);
+        } catch (e) {
+            cb(e, null);
         }
     },
-    deleteUser: async function(body,cb){
+    deleteUser: async function (body, cb) {
         const deletionUserId = body.userId;
-        try{
+        try {
             const user = await usermodel.update({
-                is_archive:true
-            },{
-                where: {
-                    "userId": deletionUserId
-                }
-            });
-            cb(null,true);
-        }catch (e) {
-            cb(e,null)
+                is_archive: true
+            }, {
+                    where: {
+                        "userId": deletionUserId
+                    }
+                });
+            cb(null, true);
+        } catch (e) {
+            cb(e, null)
         }
 
     },
-    getProfileById: async function (body,cb) {
+    getProfileById: async function (body, cb) {
         const userId = body["userId"];
-        try{
+        try {
             const profile = await usermodel.findAll({
-                where:{
-                    userId:userId,
+                where: {
+                    userId: userId,
                 },
-                attributes: { exclude: ['password_hash','is_archive','createdAt','updatedAt'] }
+                attributes: { exclude: ['password_hash', 'is_archive', 'createdAt', 'updatedAt'] }
             });
-            cb(null,profile[0])
-        }catch (e) {
-            cb(e,null);
+            cb(null, profile[0])
+        } catch (e) {
+            cb(e, null);
         }
     },
-    updateProfileById: async function(body,cb){
-        const checkpassword = async function(oldpassword){
-            const oldpassword_hash = await usermodel.findById(body.userId,{
+    updateProfileById: async function (body, cb) {
+        const checkpassword = async function (oldpassword) {
+            const oldpassword_hash = await usermodel.findById(body.userId, {
                 attributes: ['password_hash']
             });
-            const bool =  await bcrypt.compare(oldpassword,oldpassword_hash.password_hash);
-            console.log("bool :",bool);
+            const bool = await bcrypt.compare(oldpassword, oldpassword_hash.password_hash);
+            console.log("bool :", bool);
             return bool;
         };
 
-        try{
+        try {
             delete body.password_hash;
-            if(body.oldpassword && body.newpassword){
-                if(!await checkpassword(body.oldpassword)){
-                    cb(null,"Invalid current password");
+            if (body.oldpassword && body.newpassword) {
+                if (!await checkpassword(body.oldpassword)) {
+                    cb(null, "Invalid current password");
                     return;
-                }else{
+                } else {
                     body.password_hash = await bcrypt.hash(body.newpassword, 10);
                     delete body.oldpassword;
                     delete body.newpassword;
                 }
-            }else{
+            } else {
                 delete body.oldpassword;
                 delete body.newpassword;
             }
 
             const rows = await usermodel.upsert(body);
-            cb(null,"User Profile Updated Successfully");
-        }catch (e) {
-            cb(null,e);
+            cb(null, "User Profile Updated Successfully");
+        } catch (e) {
+            cb(null, e);
         }
     },
-    getPurchaseHistory: function(body,cb){
+    getPurchaseHistory: function (body, cb) {
         let sql2 = "select no_of_seats,total_price,movie_name,photos,hall_name from billings AS bill " +
             "INNER JOIN movie_schedules as schedule on bill.movie_schedule_id=schedule.id " +
             "INNER JOIN halls on schedule.hall_id=halls.id " +
@@ -112,12 +113,12 @@ module.exports = {
 
         sql2 = sql2.replace('<userId>', body["userId"]);
 
-        try{
+        try {
             usermodel.sequelize.query(sql2).spread((results, metadata) => {
-                cb(null,results);
+                cb(null, results);
             });
-        }catch (err) {
-            cb(err,null);
+        } catch (err) {
+            cb(err, null);
         }
     }
 };
